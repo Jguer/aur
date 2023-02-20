927b46f4b33fc60d5f186863e73c5a4217e379f8
jguer <me@jguer.space>
2023-02-20 19:43:31 +0100

    compile query

--- metadata/query.go
+++ metadata/query.go
@@ -28,37 +28,32 @@ func (a *Client) Get(ctx context.Context, query *aur.Query) ([]aur.Pkg, error) {
 	return found, nil
 }
 
-func (a *Client) gojqGetBatch(ctx context.Context, query *aur.Query) ([]aur.Pkg, error) {
+func (a *Client) gojqGet(ctx context.Context, needle string, contains bool, by aur.By) (gojq.Iter, error) {
 	pattern := ".[] | select("
 
-	for i, searchTerm := range query.Needles {
-		if i != 0 {
-			pattern += joiner
+	bys := toSearchBy(by)
+	for j, byString := range bys {
+		if contains && by != aur.Provides {
+			pattern += fmt.Sprintf("(.%s // empty | test(%q))", byString, needle)
+		} else {
+			pattern += fmt.Sprintf("(.%s == %q)", byString, needle)
 		}
 
-		bys := toSearchBy(query.By)
-		for j, by := range bys {
-			if query.Contains && query.By != aur.Provides {
-				pattern += fmt.Sprintf("(.%s // empty | test(%q))", by, searchTerm)
-			} else {
-				pattern += fmt.Sprintf("(.%s == %q)", by, searchTerm)
-			}
-
-			if j != len(bys)-1 {
-				pattern += joiner
-			}
+		if j != len(bys)-1 {
+			pattern += joiner
 		}
 	}
 
 	pattern += ")"
 
-	if a.debugLoggerFn != nil {
-		a.debugLoggerFn("AUR metadata query", pattern)
-	}
-
 	parsed, err := gojq.Parse(pattern)
 	if err != nil {
 		return nil, fmt.Errorf("unable to parse query: %w", err)
+	}
+
+	compiled, err := gojq.Compile(parsed)
+	if err != nil {
+		return nil, fmt.Errorf("unable to compile query: %w", err)
 	}
 
 	unmarshalledCache, errCache := a.cache(ctx)
@@ -66,29 +61,45 @@ func (a *Client) gojqGetBatch(ctx context.Context, query *aur.Query) ([]aur.Pkg,
 		return nil, errCache
 	}
 
+	if a.debugLoggerFn != nil {
+		a.debugLoggerFn("AUR metadata query", pattern)
+	}
+
+	iter := compiled.RunWithContext(ctx, unmarshalledCache)
+
+	return iter, nil
+}
+
+func (a *Client) gojqGetBatch(ctx context.Context, query *aur.Query) ([]aur.Pkg, error) {
 	final := make([]aur.Pkg, 0, len(query.Needles))
-	iter := parsed.RunWithContext(ctx, unmarshalledCache) // or query.RunWithContext
 	dedup := make(map[string]bool)
 
-	for pkgMap, ok := iter.Next(); ok; pkgMap, ok = iter.Next() {
-		if err, ok := pkgMap.(error); ok {
+	for _, needle := range query.Needles {
+		iter, err := a.gojqGet(ctx, needle, query.Contains, query.By)
+		if err != nil {
 			return nil, err
 		}
 
-		name := pkgMap.(map[string]interface{})["Name"].(string)
-		if dedup[name] {
-			continue
+		for pkgMap, ok := iter.Next(); ok; pkgMap, ok = iter.Next() {
+			if err, ok := pkgMap.(error); ok {
+				return nil, err
+			}
+
+			name := pkgMap.(map[string]interface{})["Name"].(string)
+			if dedup[name] {
+				continue
+			}
+
+			dedup[name] = true
+
+			pkg := aur.Pkg{}
+			errU := mapstructure.Decode(pkgMap, &pkg)
+			if errU != nil {
+				return nil, fmt.Errorf("unable to decode aur package: %w: %+v", errU, pkgMap.(map[string]interface{}))
+			}
+
+			final = append(final, pkg)
 		}
-
-		dedup[name] = true
-
-		pkg := aur.Pkg{}
-		errU := mapstructure.Decode(pkgMap, &pkg)
-		if errU != nil {
-			return nil, fmt.Errorf("unable to decode aur package: %w: %+v", errU, pkgMap.(map[string]interface{}))
-		}
-
-		final = append(final, pkg)
 	}
 
 	if a.debugLoggerFn != nil {
